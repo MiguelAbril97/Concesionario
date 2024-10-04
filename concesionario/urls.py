601bf530ab580f4f1044b2789ae3041eeafb742f
Miguel Abril <miguel.abril.alamo.al@iespoligonosur.org>
2024-10-04 13:47:28 +0200

    añado url vacia

--- concesionario/urls.py
+++ concesionario/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.verVendedores, name='verVendedores.html'),
     path('vendedor/', views.verVendedores, name='verVendedores.html'),
     path('coches/', views.verCoches, name='verCoches.html'),
     path('ventas/', views.verVentas, name='verVentas.html'),
